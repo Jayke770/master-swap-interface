9b500cc4ab19f74804125680536325fe12601543
Jayke770 <jakevelasco40@gmail.com>
2024-06-23 11:37:56 +0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -17,16 +17,27 @@ import {
   DropdownItem,
   Card,
   Input,
-  Image
+  Image,
+  Modal,
+  useDisclosure,
+  ModalContent,
+  ModalHeader,
+  ModalBody
 } from "@nextui-org/react";
 import { useLocalstorageState } from 'rooks'
 import * as lodash from 'lodash'
 import { useForm, SubmitHandler } from "react-hook-form"
 import { motion } from 'framer-motion'
+import { Search } from 'lucide-react';
 const tradeTabs = ["swap", "send", "buy"]
 export default function Home() {
+  const {
+    isOpen: isOpenToken,
+    onOpen: onOpenTokens,
+    onClose: onCloseTokens } = useDisclosure();
   const [tradeTab, setTradeTab] = useState<"swap" | "send" | "buy">("swap")
   const onSetTradeTab = (tab: "swap" | "send" | "buy") => setTradeTab(tab)
+  const onToggleTokensModal = () => isOpenToken ? onCloseTokens() : onOpenTokens()
   return (
     <>
       <Navbar isBlurred isBordered maxWidth="xl">
@@ -47,20 +58,127 @@ export default function Home() {
         </NavbarContent>
       </Navbar>
 
-      <div className="flex flex-col w-full h-full mt-10 p-4">
-        <div className="flex gap-2">
-          {tradeTabs.map(tab => (
-            <Button
-              key={tab}
-              radius="lg"
-              color={tradeTab === tab ? "primary" : "default"}
-              onClick={() => onSetTradeTab(tab as any)}
-              variant={tradeTab === tab ? "shadow" : "flat"}>{lodash.capitalize(tab)}</Button>
-          ))}
-        </div>
-        {tradeTab === "swap" && (
-          <div className="flex flex-col gap-2 w-full">
-            <div className="flex flex-col gap-2 mt-5">
+      <section className='flex justify-center items-center w-full h-full mt-5 md:mt-10'>
+        <div className="flex flex-col w-full md:w-[500px] h-full p-4">
+          <motion.div
+            initial={{
+              opacity: 0
+            }}
+            animate={{
+              opacity: 1
+            }}
+            exit={{
+              opacity: 0
+            }}
+            transition={{ type: "spring", duration: 0.9 }}
+            className="flex gap-2">
+            {tradeTabs.map(tab => (
+              <Button
+                key={tab}
+                radius="lg"
+                color={tradeTab === tab ? "primary" : "default"}
+                onClick={() => onSetTradeTab(tab as any)}
+                variant={tradeTab === tab ? "shadow" : "flat"}>{lodash.capitalize(tab)}</Button>
+            ))}
+          </motion.div>
+          {tradeTab === "swap" && (
+            <div className="flex flex-col gap-2 w-full">
+              <div className="flex flex-col gap-2 mt-5">
+                <motion.div
+                  initial={{
+                    scale: 0.8,
+                    opacity: 0
+                  }}
+                  animate={{
+                    scale: 1,
+                    opacity: 1
+                  }}
+                  exit={{
+                    scale: 0.8,
+                    opacity: 0
+                  }}
+                  transition={{ type: "spring", duration: 0.50, delay: 0.1 }}>
+                  <Input
+                    type="text"
+                    label="Sell"
+                    variant="bordered"
+                    defaultValue="0"
+                    placeholder="0"
+                    pattern="^[0-9]*[.,]?[0-9]*$"
+                    inputMode="decimal"
+                    autoCorrect="off"
+                    autoComplete="off"
+                    spellCheck="false"
+                    classNames={{
+                      label: "text-lg",
+                      inputWrapper: "h-28 px-4 pb-4",
+                      input: "text-3xl"
+                    }}
+                    endContent={
+                      <Button
+                        variant="flat"
+                        color="primary"
+                        startContent={
+                          <Image
+                            className="w-5 h-5"
+                            radius="full"
+                            removeWrapper
+                            src="https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png"
+                            alt="coin" />
+                        }
+                        onClick={onToggleTokensModal}>
+                        USDC
+                      </Button>
+                    } />
+                </motion.div>
+                <motion.div
+                  initial={{
+                    scale: 0.8,
+                    opacity: 0
+                  }}
+                  animate={{
+                    scale: 1,
+                    opacity: 1
+                  }}
+                  exit={{
+                    scale: 0.8,
+                    opacity: 0
+                  }}
+                  transition={{ type: "spring", duration: 0.50, delay: 0.2 }}>
+                  <Input
+                    type="text"
+                    label="Buy"
+                    variant="bordered"
+                    defaultValue="0"
+                    placeholder="0"
+                    pattern="^[0-9]*[.,]?[0-9]*$"
+                    inputMode="decimal"
+                    autoCorrect="off"
+                    autoComplete="off"
+                    spellCheck="false"
+                    classNames={{
+                      label: "text-lg",
+                      inputWrapper: "h-28 px-4 pb-4",
+                      input: "text-3xl"
+                    }}
+                    endContent={
+                      <Button
+                        variant="flat"
+                        color="primary"
+                        startContent={
+                          <Image
+                            className="w-5 h-5"
+                            radius="full"
+                            removeWrapper
+                            src="https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png"
+                            alt="coin" />
+                        }
+                        onClick={onToggleTokensModal}>
+                        USDC
+                      </Button>
+                    } />
+                </motion.div>
+              </div>
               <motion.div
                 initial={{
                   scale: 0.8,
@@ -74,112 +192,39 @@ export default function Home() {
                   scale: 0.8,
                   opacity: 0
                 }}
-                transition={{ type: "spring", duration: 0.50 }}>
-                <Input
-                  type="text"
-                  label="Sell"
-                  variant="bordered"
-                  defaultValue="0"
-                  placeholder="0"
-                  pattern="^[0-9]*[.,]?[0-9]*$"
-                  inputMode="decimal"
-                  autoCorrect="off"
-                  autoComplete="off"
-                  spellCheck="false"
-                  classNames={{
-                    label: "text-lg",
-                    inputWrapper: "h-28 px-4 pb-4",
-                    input: "text-3xl"
-                  }}
-                  endContent={
-                    <Button
-                      variant="flat"
-                      color="primary"
-                      startContent={
-                        <Image
-                          className="w-5 h-5"
-                          radius="full"
-                          removeWrapper
-                          src="https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png"
-                          alt="coin" />
-                      }>
-                      USDC
-                    </Button>
-                  } />
-              </motion.div>
-              <motion.div
-                initial={{
-                  scale: 0.8,
-                  opacity: 0
-                }}
-                animate={{
-                  scale: 1,
-                  opacity: 1
-                }}
-                exit={{
-                  scale: 0.8,
-                  opacity: 0
-                }}
-                transition={{ type: "spring", duration: 0.50, delay: 0.1 }}>
-                <Input
-                  type="text"
-                  label="Buy"
-                  variant="bordered"
-                  defaultValue="0"
-                  placeholder="0"
-                  pattern="^[0-9]*[.,]?[0-9]*$"
-                  inputMode="decimal"
-                  autoCorrect="off"
-                  autoComplete="off"
-                  spellCheck="false"
-                  classNames={{
-                    label: "text-lg",
-                    inputWrapper: "h-28 px-4 pb-4",
-                    input: "text-3xl"
-                  }}
-                  endContent={
-                    <Button
-                      variant="flat"
-                      color="primary"
-                      startContent={
-                        <Image
-                          className="w-5 h-5"
-                          radius="full"
-                          removeWrapper
-                          src="https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png"
-                          alt="coin" />
-                      }>
-                      USDC
-                    </Button>
-                  } />
+                transition={{ type: "spring", duration: 0.50, delay: 0.3 }}
+                className=' w-full'>
+                <Button
+                  className="mt-3"
+                  size="lg"
+                  variant="shadow"
+                  color="primary"
+                  fullWidth
+                  type="submit">Swap</Button>
               </motion.div>
             </div>
-            <motion.div
-              initial={{
-                scale: 0.8,
-                opacity: 0
-              }}
-              animate={{
-                scale: 1,
-                opacity: 1
-              }}
-              exit={{
-                scale: 0.8,
-                opacity: 0
-              }}
-              transition={{ type: "spring", duration: 0.50, delay: 0.2 }}
-              className=' w-full'>
-              <Button
-                className="mt-3"
-                size="lg"
-                variant="shadow"
-                color="primary"
-                fullWidth
-                type="submit">Swap</Button>
-            </motion.div>
-          </div>
-        )}
-      </div>
+          )}
+
+        </div>
+      </section>
+
+      <Modal
+        size='xl'
+        isOpen={isOpenToken}
+        scrollBehavior='inside'
+        onClose={onToggleTokensModal}>
+        <ModalContent>
+          <ModalHeader>Select Token</ModalHeader>
+          <ModalBody>
+            <Input
+              placeholder='Search Token'
+              variant='bordered'
+              color='primary'
+              size='lg'
+              startContent={<Search strokeWidth={"1.5"} />} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
