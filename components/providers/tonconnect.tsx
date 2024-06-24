e8bacd31894a8157de43cd5be744e192e8ebd204
Jayke770 <jakevelasco40@gmail.com>
2024-06-24 08:07:10 +0800

    .

--- components/providers/tonconnect.tsx
+++ components/providers/tonconnect.tsx
@@ -1,14 +1,15 @@
 "use client"
 import { TonConnectUIProvider, THEME } from '@tonconnect/ui-react';
 import { type ReactNode } from 'react';
+import { envClientConfig } from '@/lib/config'
 export default function TonProvider({ children }: { children: ReactNode }) {
     return (
         <TonConnectUIProvider
             uiPreferences={{ theme: THEME.DARK }}
             restoreConnection
             enableAndroidBackHandler
-            manifestUrl='https://66n9855d-3000.asse.devtunnels.ms/tonconnect-manifest.json'>
+            manifestUrl={`${envClientConfig.TONCONNECT_URL}/tonconnect-manifest.json`}>
             {children}
         </TonConnectUIProvider>
     )
-}
+}   
---
+++ lib/config.ts
@@ -0,0 +1,3 @@
+export const envClientConfig = {
+    TONCONNECT_URL: process.env.NEXT_PUBLIC_TONCONNECT_URL
+}
